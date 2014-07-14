0e63869575924e2ecfed8a0ea1a839f97e5501b1
otsuki <yhduettro@yahoo.co.jp>
2014-07-14 11:12:45 +0900

    All Files Added for vdchecker

---
+++ vdchecker/ViewControllers/AboutViewController.m
@@ -0,0 +1,58 @@
+//
+//  AboutViewController.m
+//  PageViewDemo
+//
+//  Created by younghwan moon on 6/29/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    UINavigationBar *naviBar = [self.navigationController navigationBar];
+    UIImage *img = [UIImage imageNamed:@"topbar_about.png"];
+    [naviBar setBackgroundImage:img forBarMetrics:UIBarMetricsDefault];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
